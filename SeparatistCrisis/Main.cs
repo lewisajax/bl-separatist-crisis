dff3a93f18bd3ca3b1e236009ea33307263b07cd
cYREXE <70070099+cYREXE@users.noreply.github.com>
2020-09-14 21:27:46 +0200

    Add files via upload

---
+++ SeparatistCrisis/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+
+namespace SeparatistCrisis
+{
+    public class Main : MBSubModuleBase
+    {
+
+        protected override void OnSubModuleLoad()
+        {
+
+        }
+
+    }
+}
