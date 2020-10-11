aebf4cad8d255a2aae6ea245841f6aa7ff49af65
AJannik <jannik.schmidberger@gmail.com>
2020-10-11 12:56:35 +0200

    Added the possibility to load custom maps in the custom battle game mode The game is now looking for a "custom_battle_scenes.xml" file in ModuleData.

--- Source/SeparatistCrisis/Main.cs
+++ Source/SeparatistCrisis/Main.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using TaleWorlds.Localization;
@@ -9,6 +10,7 @@ using TaleWorlds.MountAndBlade;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
 using TaleWorlds.ObjectSystem;
+using TaleWorlds.MountAndBlade.CustomBattle;
 
 
 namespace SeparatistCrisis
@@ -18,9 +20,23 @@ namespace SeparatistCrisis
 
         protected override void OnSubModuleLoad()
         {
-            
+
         }
-        
+
+        //This allows us to add custom maps to the custom battle mode
+        //Loading custom_battle_scenes.xml in our module
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            if (!(game.GameType is CustomGame))
+            {
+                return;
+            }
+            CustomGame customGame = (CustomGame)game.GameType;
+            
+            if(File.Exists("../../Modules/SeparatistCrisis/ModuleData/custom_battle_scenes.xml"))
+                customGame.LoadCustomBattleScenes("../../Modules/SeparatistCrisis/ModuleData/custom_battle_scenes.xml");
+        }
+
         /*
         #####This code changes the ruling clan of the southern empire on campaign start######
         public override void OnGameInitializationFinished(Game game)
