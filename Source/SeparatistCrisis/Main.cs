76f656efabbe817d4d236943ffc5d37b495f05f8
AJannik <jannik.schmidberger@gmail.com>
2021-06-06 11:03:07 +0200

    SC-1 * removed unused imports

--- Source/SeparatistCrisis/Main.cs
+++ Source/SeparatistCrisis/Main.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using TaleWorlds.MountAndBlade;
-
-using TaleWorlds.CampaignSystem;
-using TaleWorlds.Core;
-using TaleWorlds.MountAndBlade.CustomBattle;
-using Debug = System.Diagnostics.Debug;
+﻿using TaleWorlds.MountAndBlade;
 
 namespace SeparatistCrisis
 {
