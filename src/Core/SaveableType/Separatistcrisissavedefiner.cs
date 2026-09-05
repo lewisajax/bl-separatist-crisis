5f8198c741778a3128873a1443eb1e600e687184
soloman <verobusiness5123@gmail.com>
2026-09-05 13:06:34 -0700

    save type

---
+++ src/Core/SaveableType/Separatistcrisissavedefiner.cs
@@ -0,0 +1,20 @@
+﻿using SeparatistCrisis.Components;
+using TaleWorlds.SaveSystem;
+
+namespace SeparatistCrisis
+{
+    public class SeparatistCrisisSaveDefiner : SaveableTypeDefiner
+    {
+        public SeparatistCrisisSaveDefiner() : base(700001)
+        {
+        }
+
+        /// <summary>
+        /// Registers custom classes that need to be saved.
+        /// </summary>
+        protected override void DefineClassTypes()
+        {
+            AddClassDefinition(typeof(SettlementGroupComponent), 1);
+        }
+    }
+}
